77ac5c2f9eda49d5336da6405760a0940330dd32
Abdelali laatamli <abdelalilaatamli@gmail.com>
2020-12-17 00:00:17 +0100

    run main.fxml

--- src/main/java/JavaFxApplication.java
+++ src/main/java/JavaFxApplication.java
@@ -1,20 +1,51 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
 public class JavaFxApplication extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
+        //System.out.println(this.getClass().getClassLoader().getResource("views/Main.fxml"));
+
+
+        Parent root = FXMLLoader.load(this.getClass().getResource("views/Main.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+
+        /*
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
+        */
+        //System.out.println(getClass().getResource("/Main.fxml"));
+       // URL url = getClass().getResource("/Main.fxml") ;
+      //  System.out.println( url );
+       // Parent root  = FXMLLoader.load(url);
+        /*
+        Parent root = null;
+        try {
+
+            root = FXMLLoader.load(getClass().getResource("/views/Main.fxml"));
+            stage.setTitle("Hello World");
+            stage.setScene(new Scene(root, 300, 275));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+
+
     }
 
     public static void main(String[] args) {
