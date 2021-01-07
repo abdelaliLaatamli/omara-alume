2710c26f9f7ebdc72a0a8e8f19f410627ad7c608
Abdelali laatamli <abdelalilaatamli@gmail.com>
2021-01-07 22:32:36 +0100

    refactor Glass

--- src/main/java/main/Controllers/AddItemsToStockController.java
+++ src/main/java/main/Controllers/AddItemsToStockController.java
@@ -1,16 +1,21 @@
 package main.Controllers;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
 import main.Models.entities.*;
 import main.Models.enums.ProductsType;
+import main.Models.utils.CurrentCrudOperation;
 import main.Services.AccessoryService;
 import main.Services.AluminumService;
 import main.Services.GlassService;
@@ -39,6 +44,21 @@ public class AddItemsToStockController implements Initializable {
 
 
     StockEntity stockEntity ;
+
+    @FXML TableView<StockItemsEntity> tableProductsOfStockOrder;
+    @FXML TableColumn<StockItemsEntity , String> sProductName;
+    @FXML TableColumn<StockItemsEntity , String> sProductsQuentityColumn;
+    @FXML TableColumn<StockItemsEntity , String> sPriceOfBuyColumn;
+    @FXML TableColumn<StockItemsEntity , String> sTotalOrder;
+    @FXML TableColumn<StockItemsEntity , Void> sEditProductItem;
+    @FXML TableColumn<StockItemsEntity , Void> sRemoveProductFromStockItems;
+
+    ObservableList<StockItemsEntity> observableList = FXCollections.observableArrayList();
+
+
+
+    CurrentCrudOperation currentOperationStockItem = CurrentCrudOperation.ADD;
+    StockItemsEntity currentStockItemEdited = null ;
 
 
     @Override
@@ -77,6 +97,147 @@ public class AddItemsToStockController implements Initializable {
 
     }
 
+    private void loadDataTable() {
+
+        observableList.clear();
+        observableList.addAll( stockEntity.getStockItems() );
+
+        sProductName.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(  cellData.getValue().getArticle().getName() ));
+        sPriceOfBuyColumn.setCellValueFactory(new PropertyValueFactory<>("priceOfBuy"));
+        sProductsQuentityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        sTotalOrder.setCellValueFactory( cellData ->  new ReadOnlyObjectWrapper( cellData.getValue().getQuantity() * cellData.getValue().getPriceOfBuy() + " DH" ) );
+
+
+        Callback<TableColumn<StockItemsEntity, Void>, TableCell<StockItemsEntity, Void>> editCellFactory = new Callback<TableColumn<StockItemsEntity, Void>, TableCell<StockItemsEntity, Void>>() {
+            @Override
+            public TableCell<StockItemsEntity, Void> call(final TableColumn<StockItemsEntity, Void> param) {
+                final TableCell<StockItemsEntity, Void> cell = new TableCell<StockItemsEntity, Void>() {
+
+                    private final Button btn = new Button("Edit");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            StockItemsEntity data = getTableView().getItems().get(getIndex());
+
+                            switch (data.getArticle().getType()){
+                                case "AluminumEntity"  :
+                                    sProduitType.getSelectionModel().select(0);
+                                    break;
+
+                                case "AccessoryEntity" :
+                                    sProduitType.getSelectionModel().select(1);
+                                    break;
+
+                                case "GlassEntity"     :
+                                    sProduitType.getSelectionModel().select(2);
+                                    break;
+
+                                default:
+                                    System.out.println(" there no type of avalaibele types ");
+                            }
+
+                            sProduit.getSelectionModel().select(data.getArticle());
+                            sQuentity.setText( String.valueOf( data.getQuantity()) );
+                            sPriceOfBuy.setText( String.valueOf( data.getPriceOfBuy() ) );
+
+                            currentStockItemEdited = data;
+                            currentOperationStockItem = CurrentCrudOperation.EDIT;
+
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        Callback<TableColumn<StockItemsEntity, Void>, TableCell<StockItemsEntity, Void>> deleteCellFactory = new Callback<TableColumn<StockItemsEntity, Void>, TableCell<StockItemsEntity, Void>>() {
+            @Override
+            public TableCell<StockItemsEntity, Void> call(final TableColumn<StockItemsEntity, Void> param) {
+                final TableCell<StockItemsEntity, Void> cell = new TableCell<StockItemsEntity, Void>() {
+
+                    private final Button btn = new Button("Delete");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            StockItemsEntity data = getTableView().getItems().get(getIndex());
+                            System.out.println("selectedData delete: " + data.getId());
+                            stockEntity.getStockItems().remove( data );
+                            loadDataTable();
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        sEditProductItem.setCellFactory( editCellFactory );
+        sRemoveProductFromStockItems.setCellFactory( deleteCellFactory );
+
+        tableProductsOfStockOrder.setItems( this.observableList );
+
+
+    }
+
+
+    public void saveArticleInStock(MouseEvent mouseEvent) {
+
+
+
+
+
+
+        if( currentOperationStockItem == CurrentCrudOperation.ADD ){
+            StockItemsEntity stockItemsEntity = new StockItemsEntity();
+            stockItemsEntity.setArticle( sProduit.getSelectionModel().getSelectedItem());
+            stockItemsEntity.setQuantity( Float.valueOf( sQuentity.getText()) );
+            stockItemsEntity.setPriceOfBuy( Float.valueOf( sPriceOfBuy.getText() ) );
+            stockEntity.getStockItems().add( stockItemsEntity );
+        }else{
+            StockItemsEntity stockItemsEntity = currentStockItemEdited;
+            stockItemsEntity.setArticle( sProduit.getSelectionModel().getSelectedItem());
+            stockItemsEntity.setQuantity( Float.valueOf( sQuentity.getText()) );
+            stockItemsEntity.setPriceOfBuy( Float.valueOf( sPriceOfBuy.getText() ) );
+            currentStockItemEdited = null ;
+
+        }
+
+        loadDataTable();
+        currentOperationStockItem = CurrentCrudOperation.ADD;
+        sProduitType.getSelectionModel().select(0);
+        sProduit.getSelectionModel().select(null);
+        sQuentity.setText( "1" );
+        sPriceOfBuy.setText( "0" );
+
+    }
+
+
+
+    public void saveStockOrder(MouseEvent mouseEvent) {
+
+
+    }
+
+
     public void goBack(MouseEvent mouseEvent) throws IOException {
 
         Parent root = FXMLLoader.load(this.getClass().getResource("/main/views/StockView.fxml"));
@@ -85,26 +246,6 @@ public class AddItemsToStockController implements Initializable {
         main.JavaFxApplication.mainStage.show();
     }
 
-    public void saveArticleInStock(MouseEvent mouseEvent) {
-
-        StockItemsEntity stockItemsEntity = new StockItemsEntity();
-
-        stockItemsEntity.setArticle( sProduit.getSelectionModel().getSelectedItem());
-        stockItemsEntity.setQuantity( Float.valueOf( sQuentity.getText()) );
-        stockItemsEntity.setPriceOfBuy( Float.valueOf( sPriceOfBuy.getText() ) );
-        stockEntity.getStockItems().add( stockItemsEntity );
-
-
-        sProduitType.getSelectionModel().select(0);
-        sProduit.getSelectionModel().select(-1);
-        sQuentity.setText( "1" );
-        sPriceOfBuy.setText( "0" );
-
-
-    }
-
-    public void saveStockOrder(MouseEvent mouseEvent) {
-    }
 
 
 }
