e159bf60879b74c9cd06d92bb858d2c04f89cbf6
Abdelali laatamli <abdelalilaatamli@gmail.com>
2021-01-07 23:20:33 +0100

    show stock orders

--- src/main/java/main/Controllers/StockController.java
+++ src/main/java/main/Controllers/StockController.java
@@ -1,13 +1,136 @@
 package main.Controllers;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
+import main.Models.entities.OrderEntity;
+import main.Models.entities.StockEntity;
+import main.Services.StockService;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-public class StockController {
+public class StockController implements Initializable {
+
+    final StockService stockService = new StockService();
+
+    @FXML TableView<StockEntity> tableOfStockOrders;
+    @FXML TableColumn<StockEntity , String> sNameOfStock;
+    @FXML TableColumn<StockEntity , String> sProviderName;
+    @FXML TableColumn<StockEntity , String> sTotalOrder;
+    @FXML TableColumn<StockEntity , String> numberItems;
+    @FXML TableColumn<StockEntity , String> importAt;
+    @FXML TableColumn<StockEntity , Void> editProductOfCommand;
+    @FXML TableColumn<StockEntity , Void> deleteProductOfCommand;
+
+    ObservableList<StockEntity> observableList = FXCollections.observableArrayList();
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadData();
+    }
+
+    void loadData(){
+
+
+        tableOfStockOrders.getItems().clear();
+        observableList.setAll( stockService.getAll() );
+
+        sNameOfStock.setCellValueFactory( new PropertyValueFactory<>("name") );
+        sProviderName.setCellValueFactory( cellData -> new ReadOnlyStringWrapper( cellData.getValue().getProvider().getName() ) );
+        sTotalOrder.setCellValueFactory( cellData -> new ReadOnlyObjectWrapper(
+                cellData.getValue().getStockItems().stream().map( c -> c.getPriceOfBuy() * c.getQuantity() ).reduce( 0f , (subTotal , currentItem) -> subTotal + currentItem ) + " DH"
+        ) );
+        numberItems.setCellValueFactory( cellData -> new ReadOnlyObjectWrapper( cellData.getValue().getStockItems().size() ) );
+        importAt.setCellValueFactory( cellData -> new ReadOnlyObjectWrapper(
+                DateTimeFormatter.ofPattern( "dd/MM/yyyy" ).withZone(ZoneId.systemDefault()).format(cellData.getValue().getImportedAt())
+        ) );
+
+        Callback<TableColumn<StockEntity, Void>, TableCell<StockEntity, Void>> editCellFactory = new Callback<TableColumn<StockEntity, Void>, TableCell<StockEntity, Void>>() {
+            @Override
+            public TableCell<StockEntity, Void> call(final TableColumn<StockEntity, Void> param) {
+                final TableCell<StockEntity, Void> cell = new TableCell<StockEntity, Void>() {
+
+                    private final Button btn = new Button("Edit");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            StockEntity data = getTableView().getItems().get(getIndex());
+                            System.out.println("selectedData Edit: " + data.getId());
+
+
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+
+        Callback<TableColumn<StockEntity, Void>, TableCell<StockEntity, Void>> deleteCellFactory = new Callback<TableColumn<StockEntity, Void>, TableCell<StockEntity, Void>>() {
+            @Override
+            public TableCell<StockEntity, Void> call(final TableColumn<StockEntity, Void> param) {
+                final TableCell<StockEntity, Void> cell = new TableCell<StockEntity, Void>() {
+
+                    private final Button btn = new Button("Delete");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            StockEntity order = getTableView().getItems().get(getIndex());
+                            System.out.println("selectedData Delete: " + order.getId());
+
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        editProductOfCommand.setCellFactory( editCellFactory  );
+        deleteProductOfCommand.setCellFactory(deleteCellFactory);
+
+        tableOfStockOrders.setItems( observableList );
+
+
+    }
 
 
     public void goBack(MouseEvent mouseEvent) throws IOException {
@@ -28,4 +151,6 @@ public class StockController {
         main.JavaFxApplication.mainStage.show();
 
     }
+
+
 }
