9bbb8285140960250bf1d3a5298419d0cbfe7807
Abdelali laatamli <abdelalilaatamli@gmail.com>
2021-01-24 01:23:27 +0100

    add resoulve view

--- src/main/java/main/JavaFxApplication.java
+++ src/main/java/main/JavaFxApplication.java
@@ -6,17 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
 
-import java.io.IOException;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+
 
 public class JavaFxApplication extends Application {
 
     public static Stage mainStage ;
 
-    // private final static Logger LOGGER = Logger.getLogger(MyLogger.class.getName());
-    // Logger logger = Logger.getLogger(JavaFxApplication.class.getName());
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -37,23 +39,68 @@ public class JavaFxApplication extends Application {
 
     public static void main(String[] args) throws IOException {
 
-
-
 //        logger.severe("severe message");
-//
 //        logger.warning("warning message");
-//
 //        logger.info("info message");
-//
 //        logger.config("config message");
-//
 //        logger.fine("fine message");
-//
 //        logger.finer("finer message");
-//
 //        logger.finest("finest message");
 
-
+        JavaFxApplication.saveDb();
         launch();
+    }
+
+    private  static void saveDb() throws IOException {
+
+
+        String date = (new SimpleDateFormat("dd-MM-yyyy")).format(new Date());
+        String month = (new SimpleDateFormat("MM-yyyy")).format(new Date());
+
+        String filePath = JavaFxApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath().replace(
+                new java.io.File(JavaFxApplication.class.getProtectionDomain()
+                        .getCodeSource()
+                        .getLocation()
+                        .getPath())
+                        .getName() , "DBBKP" );
+
+        File f = new File(filePath);
+
+        if ( f.exists() && f.isDirectory() ) {
+            JavaFxApplication.createMonthDate( filePath , month , date );
+        }else{
+            if (f.mkdir())
+                JavaFxApplication.createMonthDate(filePath, month , date);
+        }
+
+    }
+
+    private static void createMonthDate(String filePath, String month, String date) throws IOException {
+
+
+        File f = new File(filePath +"/"+ month);
+
+        if ( f.exists() && f.isDirectory() ) {
+            JavaFxApplication.saveDbFile(filePath +"/"+ month , date );
+            System.out.println("existe");
+        }else{
+            if (f.mkdir()){
+                JavaFxApplication.saveDbFile(filePath +"/"+ month , date );
+                System.out.println("created");
+            }
+        }
+    }
+
+    private static void saveDbFile( String filePath  , String date ) throws IOException {
+        Process run = Runtime.getRuntime().exec(
+                "C:/xampp/mysql/bin/mysqldump --user=root --password=  omar_alum"
+        );
+        InputStream in = run.getInputStream();
+
+        in.read(new byte[in.available()]);
+
+        File targetFile = new File(filePath+"/"+date+".sql");
+
+        FileUtils.copyInputStreamToFile(in, targetFile);
     }
 }
